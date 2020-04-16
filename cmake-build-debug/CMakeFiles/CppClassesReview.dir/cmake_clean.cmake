33b88b60e65b35e6e02acd990edc43805b2c614c
Cochr <cochrantristan@gmail.com>
2020-04-16 15:39:43 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CppClassesReview.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppClassesReview.dir/main.cpp.obj"
+  "CppClassesReview.exe"
+  "CppClassesReview.exe.manifest"
+  "CppClassesReview.pdb"
+  "libCppClassesReview.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppClassesReview.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
